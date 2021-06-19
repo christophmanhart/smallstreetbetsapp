c7110c8a5a4ea9181f67018dfee75a87cfdcb6ac
christophmanhart <67199370+christophmanhart@users.noreply.github.com>
2021-06-19 21:50:15 +0200

    SSB-2: SsbShares Model abgeändert

--- lib/models/ssbshares.dart
+++ lib/models/ssbshares.dart
@@ -3,17 +3,20 @@ import 'package:smallstreetbetsapp/shared/empfehlung.dart';
 class SsbShares {
   final String name;
   final String wkn;
-  final double zielkurs;
+  final String zielkurs;
   final Empfehlung empfehlung;
+  final String id;
 
-  SsbShares({this.name, this.wkn, this.zielkurs, this.empfehlung});
+  SsbShares({this.name, this.wkn, this.zielkurs, this.empfehlung, this.id});
 }
 
 class SsbSharesData {
+  final String uid;
   final String name;
   final String wkn;
-  final double zielkurs;
+  final String zielkurs;
   final Empfehlung empfehlung;
+  final String id;
 
-  SsbSharesData({this.name, this.wkn, this.zielkurs, this.empfehlung});
+  SsbSharesData({this.uid, this.name, this.wkn, this.zielkurs, this.empfehlung, this.id});
 }
