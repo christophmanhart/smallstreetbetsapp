447e11f1b8c18b0600cc794dc367e9d0d8f34cf3
christophmanhart <67199370+christophmanhart@users.noreply.github.com>
2021-06-19 21:51:01 +0200

    SSB-2: Überflüssigen Import entfernt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:smallstreetbetsapp/models/the_user.dart';
 import 'package:smallstreetbetsapp/screens/wrapper.dart';
