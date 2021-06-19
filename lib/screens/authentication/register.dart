4783c11e4f9043ad9762b85e9bf38f77669e1f2c
christophmanhart <67199370+christophmanhart@users.noreply.github.com>
2021-06-19 21:52:36 +0200

    SSB-2: Sign in und Register angepasst

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -19,97 +19,144 @@ class _RegisterState extends State<Register> {
   bool loading = false;
 
   //text field states for mail and password
+  String name = "";
   String email = "";
   String password = "";
+  String passwordUeberpruefung = "";
   String error = "";
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Sign up to SmallStreetBets"),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text("Sign in"),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: "E-Mail"),
-                validator: (value) =>
-                    value.isEmpty ? "E-Mail Adresse fehlt" : null,
-                onChanged: (value) {
-                  setState(() => email = value.trim());
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: "Passwort"),
-                validator: (value) => value.length < 6
-                    ? "Das Passwort muss mindestens 6 Zeichen lang sein"
-                    : null,
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() => password = value.trim());
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  "Register",
-                  style: TextStyle(
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.green[400],
+            appBar: AppBar(
+              backgroundColor: Colors.green[400],
+              elevation: 0.0,
+              title: Text("Bei SSB registrieren"),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(
+                    Icons.person,
                     color: Colors.white,
                   ),
+                  label: Text("Einloggen",
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    setState(() => loading = true);
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                        email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = "Bitte eine valide E-Mail Adresse eingeben";
-                        loading = false;
-                      });
-                    }
-                  }
-                },
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                      textInputDecoration.copyWith(hintText: "Name"),
+                      validator: (value) =>
+                      value.isEmpty ? "Name fehlt" : null,
+                      onChanged: (value) {
+                        setState(() => name = value.trim());
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "E-Mail"),
+                      validator: (value) =>
+                          value.isEmpty ? "E-Mail Adresse fehlt" : null,
+                      onChanged: (value) {
+                        setState(() => email = value.trim());
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "Passwort"),
+                      validator: (value) => value.length < 6
+                          ? "Das Passwort muss mindestens 6 Zeichen lang sein"
+                          : null,
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() => password = value.trim());
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                      textInputDecoration.copyWith(hintText: "Passwort erneut eingeben"),
+                      validator: (value) => value.length < 6
+                          ? "Das Passwort muss mindestens 6 Zeichen lang sein"
+                          : null,
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() => passwordUeberpruefung = value.trim());
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Colors.green[800],
+                      child: Text(
+                        "Registrieren",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          if(password == passwordUeberpruefung) {
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password, name);
+                            if (result == null) {
+                              setState(() {
+                                error =
+                                "Bitte eine valide E-Mail Adresse eingeben";
+                                loading = false;
+                              });
+                            } else {
+                              setState(() {
+                                error =
+                                "Passwörter müssen gleich sein";
+                                loading = false;
+                              });
+                            }
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -24,91 +24,103 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Sign in to SmallStreetBets"),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text("Register"),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: "E-Mail"),
-                validator: (value) =>
-                    value.isEmpty ? "E-Mail Adresse fehlt" : null,
-                onChanged: (value) {
-                  setState(() => email = value.trim());
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: "Passwort"),
-                validator: (value) => value.length < 6
-                    ? "Das Passwort muss mindestens 6 Zeichen lang sein"
-                    : null,
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() => password = value);
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.green[400],
+            appBar: AppBar(
+              backgroundColor: Colors.green[400],
+              elevation: 0.0,
+              title: Text("Bei SSB einloggen"),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(
+                    Icons.person,
                     color: Colors.white,
                   ),
+                  label: Text(
+                    "Registrieren",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    setState(() => loading = true);
-                    dynamic result =
-                        await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = "Anmeldedaten falsch";
-                        loading = false;
-                      });
-                    }
-                  }
-                },
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "E-Mail"),
+                      validator: (value) =>
+                          value.isEmpty ? "E-Mail Adresse fehlt" : null,
+                      onChanged: (value) {
+                        setState(() => email = value.trim());
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "Passwort"),
+                      validator: (value) => value.length < 6
+                          ? "Das Passwort muss mindestens 6 Zeichen lang sein"
+                          : null,
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() => password = value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Colors.green[800],
+                      child: Text(
+                        "Einloggen",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = "Anmeldedaten falsch";
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
